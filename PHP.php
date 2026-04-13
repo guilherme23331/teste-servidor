cab8e86476b400ff481940cff5ae0f15dc0024fd
guilherme23331 <guilhermekayo685@gmail.com>
2026-04-13 12:44:35 -0400

    Update PHP.php

--- PHP.php
+++ PHP.php
@@ -12,12 +12,12 @@ $fone = '(19) 8888-9696';
 <html>
 <head>
 <title>Trabalhando com Variáveis</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-
-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
 </head>
 <body>
-<h1>Informações do Contato</h1>
-<p>Nome completo: <?php echo NOMECLIENTE;?> </p>
+<h1>66999880</h1>
+<p>kaique silva costa: <?php echo kaique ;?> </p>
 <p>Endereço: <?php echo $endereco;?> </p>
 <p><?php echo "$bairro - $estado - CEP: $cep";?> </p>
 <p> Telefone: <?php echo $fone;?></p>
